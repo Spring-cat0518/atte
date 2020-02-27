3cebdbaca9c61f3d712063fee457e7bd0c3699aa
Spring-cat0518 <1030186337@qq.com>
2020-02-27 16:08:53 +0800

    2020年2月27日 16:08:23

---
+++ src/views/Login.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      456456
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Login.vue"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Main.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      123132
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Main"
+    }
+</script>
+
+<style scoped>
+
+</style>
